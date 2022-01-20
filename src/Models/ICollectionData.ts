bd18681e67f9a7eda8f509c9ed4a8230d76eb86c
G. Morris <93727813+gam03@users.noreply.github.com>
2022-01-20 11:32:09 -0700

    Update ICollectionData.ts

--- src/Models/ICollectionData.ts
+++ src/Models/ICollectionData.ts
@@ -1,7 +1,9 @@
 import { Collection } from "discord.js-light";
 import ITextCommand from "./ITextCommand";
+import ISlashCommand from './ISlashCommand';
 
 export default interface ICollectionData {
     TextCommands: Collection<string, ITextCommand>;
     TextCommandAliases: Collection<string, string>;
+    SlashCommands: Collection<string, ISlashCommand>;
 }
